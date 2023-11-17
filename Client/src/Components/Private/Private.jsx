18e3c8f6713431ba1dd3f021f560d88dcf9463a0
David Iglesias <00047920@uca.edu.sv>
2023-11-17 17:43:21 -0600

    :lipstick: Detailing home design, post and page not found

--- Client/src/Components/Private/Private.jsx
+++ Client/src/Components/Private/Private.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate} from 'react-router-dom';
-
+import error404 from '../../assets/img/error404.png';
 export default function Private({role,children}){
     const navigate = useNavigate();
     // const context = useAPIContext();
@@ -13,11 +13,11 @@ export default function Private({role,children}){
     }
 
         return <div className="flex justify-center items-center  min-h-screen bg-purple-50">
-        <div className="flex flex-col justify-center">
-            <h2 className="text-8xl font-sans font-bold text-center mb-6 text-yellow-400 px-4"><FontAwesomeIcon icon={faExclamationCircle}/>404</h2>
-            <h3 className="text-4xl font-sans font-medium text-center py-1 px-4 mt-4">Page not found.</h3>
-            <p className="text-lg font-sans font-normal text-center py-1 px-4 mt-4">The page you are looking for doesn't exist or an other error occurred.</p>
-            <button onClick={()=>{logoutFuntion()}} type="button" className="font-sans bg-purple-400 hover:bg-purple-500 text-white font-bold py-1 px-4 mt-4 rounded-sm">Go to Login</button>
+        <div className="flex flex-col justify-center  transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0">
+        <img src={error404} class="h-80" alt="error" />
+            <h3 className="text-4xl font-sans font-medium text-center py-1 px-4 mt-4">Lo sentimos mucho, pagina no encontrada :().</h3>
+            <p className="text-lg font-sans font-normal text-center py-1 px-4 mt-4">La página que buscas no existe o se produjo otro error.</p>
+            <button onClick={()=>{logoutFuntion()}} type="button" className="font-sans bg-purple-400 hover:bg-purple-500 text-white font-bold py-1 px-4 mt-4 rounded-sm">Regresar</button>
         </div>
     </div>
 
--- Client/src/Pages/CreatePost/NewPost.jsx
+++ Client/src/Pages/CreatePost/NewPost.jsx
@@ -40,7 +40,7 @@ export default function NewPost() {
                 <label class="block text-gray-700 text-sm font-bold " for="asunto"> Imagen</label>  
                 <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
                 <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-white dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file"/>
-
+                <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
               </div>
               
               <div class="flex items-center justify-between">
