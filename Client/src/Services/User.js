cfea467d8edc5f91364dc446144382472a80ee3d
KevinMejia24 <00058620@uca.edu.sv>
2023-11-20 14:34:25 -0600

    :sparkles: Added 'findAllByUsername' service to the client

---
+++ Client/src/Services/User.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const URL = "https://dailypost-api.onrender.com/user";
+
+export const Auth = {
+    findAllByUsername: async (token, fragment) => {
+        try {
+            const data = await axios.get(`${URL}/username?fragment=${fragment}`,
+            {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                }
+            }
+            );
+            console.log(data);
+            return data.data;
+        } catch (error) {
+            console.error(error);
+            return false; 
+        }
+    },
+
+
+}
